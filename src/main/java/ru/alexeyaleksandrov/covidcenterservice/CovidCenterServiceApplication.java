4067af0267ee385f7ce55824e0e1789743522cdb
AlexeyAleksandrov <56308724234w@gmail.com>
2023-09-20 15:34:35 +0300

    Проект создан

---
+++ src/main/java/ru/alexeyaleksandrov/covidcenterservice/CovidCenterServiceApplication.java
@@ -0,0 +1,15 @@
+package ru.alexeyaleksandrov.covidcenterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidCenterServiceApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CovidCenterServiceApplication.class, args);
+    }
+
+}
