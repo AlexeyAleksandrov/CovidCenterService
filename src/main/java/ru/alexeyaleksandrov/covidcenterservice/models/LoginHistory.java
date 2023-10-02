53cc3f449f8524e80836b942c3153e1a18d96eeb
AlexeyAleksandrov <chinok73@yandex.ru>
2023-10-03 02:17:37 +0300

    Добавлена таблица истории входа

---
+++ src/main/java/ru/alexeyaleksandrov/covidcenterservice/models/LoginHistory.java
@@ -0,0 +1,31 @@
+package ru.alexeyaleksandrov.covidcenterservice.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "login_history")
+@Data
+public class LoginHistory
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Lob
+    @Column(name = "login", nullable = false)
+    private String login;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "\"time\"")
+    private LocalTime time;
+
+    @Column(name = "successful")
+    private Boolean successful;
+}
